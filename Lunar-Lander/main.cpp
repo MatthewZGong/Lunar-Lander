e66e2408211082e68c35d0212dc3ff4941a43a05
MatthewZGong <matthewzgong@gmail.com>
2023-11-04 17:04:36 -0400

    Initial Commit

---
+++ Lunar-Lander/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lunar-Lander
+//
+//  Created by Matthew Gong on 11/4/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
